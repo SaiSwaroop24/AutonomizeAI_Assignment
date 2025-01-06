5fa7c8d31d01b17abce816baec94b222b287df01
Nakka Sai Swaroop <110608083+SaiSwaroop24@users.noreply.github.com>
2025-01-06 16:44:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const axios = require('axios');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
